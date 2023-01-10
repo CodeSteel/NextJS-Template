68611d79bb0de628b2f80dd025c2cc2ceb13ccd1
Steel <steelzkz@gmail.com>
2023-01-10 16:45:02 -0500

    chore: rename styler

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,5 @@
 export { BaseComponent } from "./base-component";
-export { PageStyle } from "./pagestyle";
+export { PageStyle } from "./page-styler";
 export { Text } from "./text";
 export type { TextProps } from "./text";
 export { Footer } from "./footer";
